2b1c897c6675e8812dd1e9df808a32dccda22ed1
om <omkumar27516@gmail.com>
2026-05-27 10:18:45 +0530

    Authentication Completed

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -64,3 +64,68 @@ exports.register = async (req, res) => {
 
   }
 };
+
+
+//Login API
+exports.login = async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+
+    // Find user
+    const user = await prisma.user.findUnique({
+      where: {
+        email
+      }
+    });
+
+    // Check user exists
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid credentials"
+      });
+    }
+
+    // Compare password
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        message: "Invalid credentials"
+      });
+    }
+
+    // Generate JWT
+    const token = jwt.sign(
+      {
+        userId: user.id
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d"
+      }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email
+      }
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
+};
--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,17 +1,21 @@
 const express = require("express");
 
 const {
-  register
+  register,
+  login
 } = require("../controllers/auth.controller");
 
 const router = express.Router();
 
 router.post("/register", register);
-
+router.post("/login", login);
 router.get("/test", (req, res) => {
   res.json({
     message: "Auth route working"
   });
 });
 
+
+
 module.exports = router;
+
