b64e1eb4c9ebe5acbe9e027b79a41c6b7b6adfea
om <omkumar27516@gmail.com>
2026-05-29 21:34:08 +0530

    Fixed ai service import

--- client/src/components/ResumeUpload.jsx
+++ client/src/components/ResumeUpload.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
-import { analyzeResume }
-from "../services/aiService";
+import { analyzeResume } from "../services/aiservice";
 
 const ResumeUpload = ({
   setAnalysis
